79354ea25ccf67ec884ea5b19b34fadba00bf019
fabricio <fabriciusbr@gmail.com>
2020-05-19 16:04:06 -0300

    Line graph with input for stock symbols

--- app.py
+++ app.py
@@ -3,18 +3,12 @@ import datetime
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Output, Input
 
 app = dash.Dash()
 
 stock = 'TSLA'
 
-start = datetime.datetime(2010, 1, 1)
-end = datetime.datetime.now()
-df = web.DataReader(stock, 'yahoo', start, end)
-df.reset_index(inplace=True)
-df.set_index("Date", inplace=True)
-if "Symbol" in df.columns:
-    df = df.drop("Symbol", axis=1)
 
 app.layout = html.Div(children=[
     html.H1(children='Dashboard Example'),
@@ -23,18 +17,35 @@ app.layout = html.Div(children=[
         Stocks line graph
     '''),
 
-    dcc.Graph(
+    dcc.Input(id='input', value='', type='text'),
+    html.Div(id='output-graph')
+
+])
+
+
+@ app.callback(Output('output-graph', 'children'),
+               [Input('input', 'value')])
+def update_graph(symbol):
+    start = datetime.datetime(2010, 1, 1)
+    end = datetime.datetime.now()
+    df = web.DataReader(symbol, 'yahoo', start, end)
+    df.reset_index(inplace=True)
+    df.set_index("Date", inplace=True)
+    if "Symbol" in df.columns:
+        df = df.drop("Symbol", axis=1)
+
+    return dcc.Graph(
         id='example-graph',
         figure={
             'data': [
                 {'x': df.index, 'y': df.Close, 'type': 'line', 'name': stock},
             ],
             'layout': {
-                'title': f'{stock} stocks'
+                'title': f'{symbol} stocks'
             }
         }
     )
-])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
