b3b5cfc08545563013be800660855db5310e0d9b
fabricio <fabriciusbr@gmail.com>
2020-05-19 14:08:54 -0300

    Showing an input value in a div

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 import dash
+from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1('Dash Tutorial'),
-    dcc.Graph(id='example',
-              figure={
-                  'data': [{'x': [1, 2, 3, 4, 5],
-                            'y': [5, 6, 7, 2, 1],
-                            'type': 'line',
-                            'name': 'boats'},
-                           {'x': [1, 2, 3, 4, 5],
-                            'y': [8, 3, 2, 5, 5],
-                            'type': 'bar',
-                            'name': 'cars'}],
-                  'layout': {
-                      'title': 'Dashboard'
-                  }
-              }
-              )
+    dcc.Input(id='input', value='Enter something', type='text'),
+    html.Div(id='output')
 ])
+
+
+@app.callback(Output(component_id='output',
+                     component_property='children'),
+              [Input(component_id='input',
+                     component_property='value')],
+              )
+def update_value(input_data):
+    return f"Input: {input_data}"
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
