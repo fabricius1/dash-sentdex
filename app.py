61414b5fa27db1a0df5d22596afcdf51a0d8df1d
fabricio <fabriciusbr@gmail.com>
2020-05-19 17:08:28 -0300

    Fist line chart with update every 0.5s

--- app.py
+++ app.py
@@ -1,50 +1,52 @@
-import pandas_datareader.data as web
-import datetime
 import dash
+from dash.dependencies import Output, Event
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Output, Input
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+
+# a deque is like a list with max length
+# When the list is full and you try to append another element,
+# the first element will be popped out to give room to the new one
+
+X = deque(maxlen=20)
+Y = deque(maxlen=20)
+X.append(1)
+Y.append(1)
 
 app = dash.Dash()
-
-stock = 'TSLA'
-
-
-app.layout = html.Div(children=[
-    html.H1(children='Dashboard Example'),
-
-    html.Div(children='''
-        Stocks line graph
-    '''),
-
-    dcc.Input(id='input', value='', type='text'),
-    html.Div(id='output-graph')
-
-])
+app.layout = html.Div(
+    [
+        dcc.Graph(id='live-graph', animate=True),
+        dcc.Interval(
+            id='graph-update',
+            interval=500  # 1s
+        )
+    ]
+)
 
 
-@ app.callback(Output('output-graph', 'children'),
-               [Input('input', 'value')])
-def update_graph(symbol):
-    start = datetime.datetime(2010, 1, 1)
-    end = datetime.datetime.now()
-    df = web.DataReader(symbol, 'yahoo', start, end)
-    df.reset_index(inplace=True)
-    df.set_index("Date", inplace=True)
-    if "Symbol" in df.columns:
-        df = df.drop("Symbol", axis=1)
+@app.callback(Output('live-graph', 'figure'),
+              events=[Event('graph-update', 'interval')]
+              )
+def update_graph():
+    global X
+    global Y
+    X.append(X[-1]+1)
+    Y.append(Y[-1]+(Y[-1]*random.uniform(-0.1, 0.1)))
 
-    return dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': df.index, 'y': df.Close, 'type': 'line', 'name': stock},
-            ],
-            'layout': {
-                'title': f'{symbol} stocks'
-            }
-        }
+    data = go.Scatter(
+        x=list(X),
+        y=list(Y),
+        name='Scatter',
+        mode='lines+markers'
     )
+
+    return {'data': [data],
+            'layout': go.Layout(xaxis=dict(range=[min(X), max(X)]),
+                                yaxis=dict(range=[min(Y), max(Y)]))}
 
 
 if __name__ == '__main__':
