e6b05cef864a14cf34f463e0e4bd8c731f21bfdc
fabricio <fabriciusbr@gmail.com>
2020-05-20 15:11:45 -0300

    Dropdown with multiple selection to live updating graphs

--- app.py
+++ app.py
@@ -1,52 +1,127 @@
 import dash
-from dash.dependencies import Output, Event
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly
-import random
 import plotly.graph_objs as go
 from collections import deque
-
-# a deque is like a list with max length
-# When the list is full and you try to append another element,
-# the first element will be popped out to give room to the new one
-
-X = deque(maxlen=20)
-Y = deque(maxlen=20)
-X.append(1)
-Y.append(1)
-
-app = dash.Dash()
-app.layout = html.Div(
-    [
-        dcc.Graph(id='live-graph', animate=True),
-        dcc.Interval(
-            id='graph-update',
-            interval=500  # 1s
-        )
-    ]
-)
+import time
+import random
 
 
-@app.callback(Output('live-graph', 'figure'),
-              events=[Event('graph-update', 'interval')]
+max_length = 50
+times = deque(maxlen=max_length)
+oil_temps = deque(maxlen=max_length)
+intake_temps = deque(maxlen=max_length)
+coolant_temps = deque(maxlen=max_length)
+rpms = deque(maxlen=max_length)
+speeds = deque(maxlen=max_length)
+throttle_pos = deque(maxlen=max_length)
+
+
+dict_data = {'Oil Temperature': oil_temps,
+             'Intake Temperature': intake_temps,
+             'Coolant Temperature': coolant_temps,
+             'RPM': rpms,
+             'Speed': speeds,
+             'Throttle Position': throttle_pos}
+
+# external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
+# external_js = ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js']
+app = dash.Dash('vehicle-data')
+                # external_scripts=external_js,
+                # external_stylesheets=external_css)
+
+
+def update_obd_values(times, oil_temps, intake_temps,
+                      coolant_temps, rpms, speeds,
+                      throttle_pos):
+    times.append(time.time())
+    if len(times) == 1:
+        # starting relevant values
+        oil_temps.append(random.randrange(180, 230))
+        intake_temps.append(random.randrange(95, 115))
+        coolant_temps.append(random.randrange(170, 220))
+        rpms.append(random.randrange(1000, 9500))
+        speeds.append(random.randrange(30, 140))
+        throttle_pos.append(random.randrange(10, 90))
+    else:
+        for data_of_interest in [oil_temps, intake_temps, coolant_temps,
+                                 rpms, speeds, throttle_pos]:
+            data_of_interest.append(data_of_interest[-1]
+                                    + data_of_interest[-1]*random.uniform(-0.0001, 0.0001))
+
+    return times, oil_temps, intake_temps, coolant_temps, rpms, speeds, throttle_pos
+
+
+times, oil_temps, intake_temps, coolant_temps, rpms, speeds, throttle_pos = update_obd_values(
+    times, oil_temps, intake_temps, coolant_temps, rpms, speeds, throttle_pos)
+
+
+app.layout = html.Div([
+    html.Div([
+        html.H2('Vehicle Data',
+                style={'float': 'left'}),
+    ]),
+
+    dcc.Dropdown(id='vehicle-data-name',
+                 options=[{'label': s, 'value': s}
+                          for s in dict_data.keys()],
+                 value=['Coolant Temperature', 'Oil Temperature',
+                        'Intake Temperature'],
+                 multi=True
+                 ),
+    html.Div(children=html.Div(id='graphs'), className='row'),
+    dcc.Interval(
+        id='graph-update',
+        interval=1000,
+        n_intervals=0),
+], className='container',
+    style={'width': '98%', 'margin-left': 10,
+           'margin-right': 10, 'max-width': 500000})
+
+
+@app.callback(Output('graphs', 'children'),
+              [Input('vehicle-data-name', 'value'),
+              Input('graph-update', 'n_intervals')]
               )
-def update_graph():
-    global X
-    global Y
-    X.append(X[-1]+1)
-    Y.append(Y[-1]+(Y[-1]*random.uniform(-0.1, 0.1)))
+def update_graph(data_names, n_intervals):
+    graphs = []
 
-    data = go.Scatter(
-        x=list(X),
-        y=list(Y),
-        name='Scatter',
-        mode='lines+markers'
-    )
+    update_obd_values(times, oil_temps, intake_temps,
+                      coolant_temps, rpms, speeds, throttle_pos)
 
-    return {'data': [data],
-            'layout': go.Layout(xaxis=dict(range=[min(X), max(X)]),
-                                yaxis=dict(range=[min(Y), max(Y)]))}
+    if len(data_names) > 2:
+        class_choice = 'col s12 m6 l4'
+    elif len(data_names) == 2:
+        class_choice = 'col s12 m6 l6'
+    else:
+        class_choice = 'col s12'
+
+    for data_name in data_names:
+
+        data = go.Scatter(
+            x=list(times),
+            y=list(dict_data[data_name]),
+            name='Scatter',
+            fill='tozeroy',
+            fillcolor='#6897bb'
+        )
+
+        graphs.append(html.Div(dcc.Graph(
+            id=data_name,
+            animate=True,
+            figure={'data': [data],
+                    'layout': go.Layout(
+                xaxis=dict(range=[min(times), max(times)]),
+                yaxis=dict(range=[min(dict_data[data_name]),
+                                  max(dict_data[data_name])]),
+                margin={'l': 50, 'r': 1, 't': 45, 'b': 1},
+                title=f'{data_name}'
+            )
+            }
+        ), className=class_choice))
+
+    return graphs
 
 
 if __name__ == '__main__':
